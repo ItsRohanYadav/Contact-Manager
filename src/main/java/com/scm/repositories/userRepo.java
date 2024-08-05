0f05836c04604b9119017dbc1c659fdec7e03d76
Rohan Yadav <itsrohanyadav20@gmail.com>
2024-08-05 16:48:47 +0530

    commit

--- src/main/java/com/scm/repositories/userRepo.java
+++ src/main/java/com/scm/repositories/userRepo.java
@@ -2,9 +2,14 @@ package com.scm.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.scm.entities.user;
 
+@Repository
+
 public interface userRepo extends JpaRepository<user, String>{
+
+    public user saveUser(user user);
     
 }
